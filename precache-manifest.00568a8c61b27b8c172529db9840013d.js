a6d8eed5b7f573aa018c4ef706d3eade5ac24dac
loodnn <loudjane@163.com>
2021-06-06 16:11:32 +0800

    deploy

---
+++ precache-manifest.00568a8c61b27b8c172529db9840013d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dde55ca398c5039823f",
+    "url": "/Accountting-demo/css/app.346e558a.css"
+  },
+  {
+    "revision": "08fa1f034f5dab7d0c4d8d11c004f792",
+    "url": "/Accountting-demo/index.html"
+  },
+  {
+    "revision": "1dde55ca398c5039823f",
+    "url": "/Accountting-demo/js/app.61b557eb.js"
+  },
+  {
+    "revision": "cde56d4cb86690f282ac",
+    "url": "/Accountting-demo/js/chunk-vendors.338138ce.js"
+  },
+  {
+    "revision": "8c037bf97bc8532092c051e1d550146a",
+    "url": "/Accountting-demo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Accountting-demo/robots.txt"
+  }
+]);
